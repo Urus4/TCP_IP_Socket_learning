f05978655d05078c3be510113a55432b08227f4e
Urus4 <102502766+Urus4@users.noreply.github.com>
2022-06-25 15:06:44 +0900

    23일자 공부
    동적, 정적 라이브러리
    Makefile
    멀티쓰레드

---
+++ library/calculate.c
@@ -0,0 +1,20 @@
+#include "op_server.h"
+
+
+int calculate(int opnum, int opnds[], char op)
+ {
+    int result = opnds[0], i;
+    switch(op)
+    {
+        case '+':
+            for(i=1;i<opnum;i++) result+=opnds[i];
+            break;
+        case '-':
+            for(i=1;i<opnum;i++) result-=opnds[i];
+            break;
+        case '*':
+            for(i=1;i<opnum;i++) result*=opnds[i];
+            break;
+    }
+    return result;
+}
---
+++ library/main.c
@@ -0,0 +1,13 @@
+
+
+void print(char* str);
+char *input();
+
+int main()
+{
+	char *str = input();
+	print(str);
+	print("\n");
+
+	return 0;
+}
